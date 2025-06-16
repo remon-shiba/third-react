08df844f45a1db9e5db6ae5b23d8802681894389
Raymond Bago <raymond.bago@fortress-asya.com>
2025-06-16 16:39:52 +0800

    initial commit

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Registration from '../pages/Register';
 import ForgotPassword from '../pages/ForgotPassword';
-import Dashboard from '../pages/Dashboard'; // optional example
 
 const AppRoutes = ({ setIsLoggedIn }) => {
   return (
@@ -10,7 +9,6 @@ const AppRoutes = ({ setIsLoggedIn }) => {
       <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/dashboard" element={<Dashboard />} /> {/* dummy protected route */}
     </Routes>
   );
 };
